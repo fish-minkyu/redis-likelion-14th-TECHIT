5f5f054387441bd6fe30b6f0693d125c93c98e9e
fish-minkyu <e951219@naver.com>
2024-02-14 17:50:01 +0900

    update: 0213 1300 lecture

--- README.md
+++ README.md
@@ -19,5 +19,17 @@
 
 ## Key Point
 
+- ResdisTemplate & Value ~~ 
+
+- Configuration (PersonDto)
+
+- session Put & Get
+
+- 복수의 Application에서 session 공유 설정
+default. application.yaml Redis 연결
+build.gradle
+RedisConfig or RedisApplication
+
+
 ## Github
 [강사님 GitHub](https://github.com/edujeeho0/likelion-backend-8-redis)
--- src/main/java/com/example/redis/SessionController.java
+++ src/main/java/com/example/redis/SessionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+// 인증 외 상황(장바구니 등)에서 세션 정보를 저장하기 위한 Controller
 @Slf4j
 @RestController
 @RequestMapping("/session")
@@ -15,6 +16,7 @@ public class SessionController {
   public void setSession(
     @RequestParam("key") String key,
     @RequestParam("value") String value,
+    // Key - Value로 이뤄져 있음
     HttpSession session
   ) {
     session.setAttribute(key, value);
@@ -25,6 +27,7 @@ public class SessionController {
     @RequestParam("key") String key,
     HttpSession session
   ) {
+    // setAttribute를 Object형으로 저장하였기에 Object로 받아준다.
     Object value = session.getAttribute(key);
     if (value == null)
       throw new ResponseStatusException(HttpStatus.NOT_FOUND);
